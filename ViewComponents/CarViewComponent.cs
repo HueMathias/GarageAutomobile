97ab8effec5bc22e3a903b83b02fc6d0d7911a81
Mathias Hue <mathias.hue@hotmail.com>
2022-12-27 20:10:17 +0100

    add ViewComponent for cars list and UpMenu items

---
+++ ViewComponents/CarViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using ConstellationGarage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstellationGarage.ViewComponents
+{
+    public class CarViewComponent : ViewComponent
+    {
+        private readonly GarageautomobileContext _context;
+        public CarViewComponent(GarageautomobileContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+            return await Task.FromResult(View(_context.Cars.ToList()));
+        }
+    }
+}
---
+++ ViewComponents/UpMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using ConstellationGarage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConstellationGarage.ViewComponents
+{
+    public class UpMenuViewComponent : ViewComponent
+    {
+        private readonly GarageautomobileContext _context;
+        public UpMenuViewComponent(GarageautomobileContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var tables = _context.Model.GetEntityTypes()
+                .OrderBy(t => t.Name)
+                .ToList();
+            return await Task.FromResult(View(tables));
+        }
+    }
+}
